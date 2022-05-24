ebd50a0f433cb1701ae793c870c9b4ebc8dc1cd1
Kevin J. Delaney <kjdelaney@ucsd.edu>
2022-05-24 13:03:50 -0700

    Correcting __init__.py

--- dbmi_redcap/__init__.py
+++ dbmi_redcap/__init__.py
@@ -7,3 +7,5 @@ A wrapper around the REDCap API, allowing Python code to connect with the API.
 __version__ = "1.0.0"
 __author__ = 'DBMI Team'
 __credits__ = 'University of California San Diego School of Medicine; Department of Biomedical Informatics'
+
+from .redcap_api_interface import REDCapInterface
