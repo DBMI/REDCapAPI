3d387f7ee9659c6482b10b41c1a84ce1d2f2d2f2
Kevin J. Delaney <kjdelaney@ucsd.edu>
2022-06-02 09:49:32 -0700

    Bump version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dbmi-redcap',
-    version='1.0.0',
+    version='1.0.1',
     packages=['dbmi_redcap'],
     url='https://github.com/DBMI/REDCap_API_Calls',
     license='',
