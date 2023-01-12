b4150d4e57a62ba77baa097547a3bde790fdf9d5
Kevin J. Delaney <kjdelaney@ucsd.edu>
2023-01-12 14:18:23 -0800

    Poetry stuff.

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="redcap_api",
-    version="1.3.2",
+    version="1.3.3",
     package_dir={"": "src"},
     packages=["redcap_api"],
     url="https://github.com/DBMI/REDCapAPI",
