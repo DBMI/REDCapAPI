8c313c9083f21391431f44a07fbfa8d2015135f4
Kevin J. Delaney <kjdelaney@ucsd.edu>
2023-01-12 16:51:00 -0800

    Return to reading secrets from local file.

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="redcap_api",
-    version="1.3.4",
+    version="1.3.5",
     package_dir={"": "src"},
     packages=["redcap_api"],
     url="https://github.com/DBMI/REDCapAPI",
