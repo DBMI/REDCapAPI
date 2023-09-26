01b1a0742efabfe5f5cd63da639a25741669270d
Kevin J. Delaney <kjdelaney@ucsd.edu>
2023-09-26 15:53:50 -0700

    Added 'report()' method.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="redcapapi",
-    version="1.5.1",
+    version="1.5.2",
     package_dir={"": "src"},
     packages=["redcapapi"],
     url="https://github.com/DBMI/REDCapAPI",
