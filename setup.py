c984c3733f5dd53fa5767b0cd00f621b4615e0d0
Kevin J. Delaney <kjdelaney@UCSD.EDU>
2024-02-26 13:21:15 -0800

    Add overwrite option

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="redcapapi",
-    version="1.5.9",
+    version="1.6.0",
     package_dir={"": "src"},
     packages=["redcapapi"],
     url="https://github.com/DBMI/REDCapAPI",
