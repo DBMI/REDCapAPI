1e1d81fc1ae2889856e397f315fd0c526d92cf98
kevin-dbmi <kjdelaney@ucsd.edu>
2026-08-19 10:14:14 -0700

    Specify main rev for REDCapUtilities

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="redcapapi",
-    version="1.6.4",
+    version="1.6.5",
     package_dir={"": "src"},
     packages=["redcapapi"],
     url="https://github.com/DBMI/REDCapAPI",
